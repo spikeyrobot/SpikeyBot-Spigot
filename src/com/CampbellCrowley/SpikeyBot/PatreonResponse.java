91caee76b952d539fe7ba1819d9c3f3c38332f12
Campbell Crowley <spikeyrobot@campbellcrowley.com>
2019-07-28 15:20:32 -0700

    LuckPerms integrated, v1.0.0

--- src/com/CampbellCrowley/SpikeyBot/PatreonResponse.java
+++ src/com/CampbellCrowley/SpikeyBot/PatreonResponse.java
@@ -1,31 +1,85 @@
 package com.CampbellCrowley.SpikeyBot;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import me.lucko.luckperms.api.DataMutateResult;
+import me.lucko.luckperms.api.Group;
+import me.lucko.luckperms.api.LuckPermsApi;
+import me.lucko.luckperms.api.Node;
+import me.lucko.luckperms.api.User;
 
 public class PatreonResponse implements Callback {
   Player player;
+  boolean sendReply = false;
+  Plugin plugin;
 
-  public PatreonResponse(Player player) {
+  public PatreonResponse(Player player, Plugin plugin) {
     this.player = player;
+    this.plugin = plugin;
+  }
+
+  public PatreonResponse(Player player, Plugin plugin, boolean sendReply) {
+    this.player = player;
+    this.sendReply = sendReply;
+    this.plugin = plugin;
   }
 
   public void callback(final String res) {
-    if (res.equals("pledged")) {
-      this.player.sendMessage(ChatColor.GREEN
-          + "Thank you for supporting Spikey! Your accounts are linked.");
-    } else if (res.equals("nopledge")) {
-      this.player.sendMessage(
-          ChatColor.GREEN + "Your account is linked with Spikey's Patreon, "
-              + "but you have not pledged.");
-    } else if (res.equals("unlinked")) {
-      this.player.sendMessage(ChatColor.YELLOW
-          + "Your account is not yet linked with Spikey's Patreon."
-          + "\nLink with '/patreon link'.");
-    } else {
-      this.player.sendMessage(ChatColor.RED
-          + "Failed to check Patreon status. Please try again later.\n(" + res
-          + ")");
+    if (this.sendReply) {
+      if (res.equals("pledged")) {
+        this.player.sendMessage(ChatColor.GREEN
+            + "Thank you for supporting Spikey! Your accounts are linked.");
+      } else if (res.equals("nopledge")) {
+        this.player.sendMessage(
+            ChatColor.GREEN + "Your account is linked with Spikey's Patreon, "
+                + "but you have not pledged.");
+      } else if (res.equals("unlinked")) {
+        this.player.sendMessage(ChatColor.YELLOW
+            + "Your account is not yet linked with Spikey's Patreon."
+            + "\nLink with '/patreon link'.");
+      } else {
+        this.player.sendMessage(ChatColor.RED
+            + "Failed to check Patreon status. Please try again later.\n(" + res
+            + ")");
+      }
+    }
+    FileConfiguration config = this.plugin.getConfig();
+    String groupName = config.getString("luckperms.donator-group");
+    if (this.player.isOnline() && config.getBoolean("luckperms.enabled")
+        && groupName != null && groupName.length() > 0) {
+      boolean goalGroup = res.equals("pledged");
+
+      RegisteredServiceProvider<LuckPermsApi> provider = Bukkit
+          .getServicesManager().getRegistration(LuckPermsApi.class);
+      if (provider != null) {
+        LuckPermsApi lpApi = provider.getProvider();
+        User user = lpApi.getUserManager().getUser(this.player.getUniqueId());
+        Group group = lpApi.getGroupManager().getGroup(groupName);
+
+        if (group == null) {
+          this.plugin.getLogger().severe("Unable to find group: " + groupName);
+          return;
+        }
+
+        Node node = lpApi.getNodeFactory().makeGroupNode(group).build();
+
+        if (goalGroup) {
+          DataMutateResult result = user.setTransientPermission(node);
+          if (result != DataMutateResult.ALREADY_HAS) {
+            String message = config.getString("luckperms.give-role-message");
+            if (message != null && message.length() > 0) {
+              this.player.sendMessage(message);
+            }
+          }
+        } else if (!goalGroup) {
+          user.unsetTransientPermission(node);
+        }
+      }
     }
   }
 }
--- src/com/CampbellCrowley/SpikeyBot/SpikeyBot.java
+++ src/com/CampbellCrowley/SpikeyBot/SpikeyBot.java
@@ -1,17 +1,49 @@
 package com.CampbellCrowley.SpikeyBot;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SpikeyBot extends JavaPlugin {
+import me.lucko.luckperms.api.LuckPermsApi;
+
+public class SpikeyBot extends JavaPlugin implements Listener {
+  LuckPermsApi lpApi;
+
+  @Override
+  public void onEnable() {
+    getConfig().options().copyDefaults(true);
+    this.saveDefaultConfig();
+    
+    getServer().getPluginManager().registerEvents(this, this);
+
+    if (getConfig().getBoolean("luckperms.enabled")) {
+      RegisteredServiceProvider<LuckPermsApi> provider = Bukkit
+          .getServicesManager().getRegistration(LuckPermsApi.class);
+      if (provider != null) {
+        this.lpApi = provider.getProvider();
+      } else {
+        getLogger().warning(
+            "LuckPerms not found. Automatic role management is disabled.");
+      }
+    }
+  }
+
   @Override
   public boolean onCommand(CommandSender sender, Command cmd, String label,
       String[] args) {
     if (cmd.getName().equalsIgnoreCase("patreon")) {
       if (!(sender instanceof Player)) {
         sender.sendMessage("This command can only be run by a player.");
+        return true;
+      }
+      if (!sender.hasPermission("SB.patreon")) {
+        sender.sendMessage("You don't have permission for this command.");
         return true;
       }
       final String uuid = ((Player) sender).getUniqueId().toString()
@@ -23,13 +55,29 @@ public class SpikeyBot extends JavaPlugin {
             new TokenResponse((Player) sender)));
         t.start();
       } else {
-        Thread t = new Thread(new HttpsRequest(
-            "https://www.spikeycraft.com/fetch/patreon/" + uuid,
-            new PatreonResponse((Player) sender)));
-        t.start();
+        fetchPatreon((Player) sender, true);
       }
       return true;
     }
     return false;
+  }
+
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent evt) {
+    if (this.lpApi == null || !getConfig().getBoolean("luckperms.enabled")
+        || !getConfig().getBoolean("luckperms.update-on-login")
+        || getConfig().getString("luckperms.donator-group").length() == 0) {
+      return;
+    }
+    final Player player = evt.getPlayer();
+    fetchPatreon(player, false);
+  }
+
+  private void fetchPatreon(Player player, boolean sendReply) {
+    final String uuid = player.getUniqueId().toString().replace("-", "");
+    Thread t = new Thread(
+        new HttpsRequest("https://www.spikeycraft.com/fetch/patreon/" + uuid,
+            new PatreonResponse(player, this, sendReply)));
+    t.start();
   }
 }
